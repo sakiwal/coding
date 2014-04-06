c20672f5d610cf671be624b23348a35760e93189
David Cameron <dave@learnko.com>
2014-04-06 13:45:29 +1200

    You're welcome.

---
+++ adder.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+# Find number provided at command prompt
+# then convert to an integer
+n = sys.argv[1]
+n = int(n)
+
+
+# Figured out strange algorithm; results in 100x
+# speedup
+result = n/2 * (n+1)
+print "XXSum", result
+
+
+# calculate the sum from 1 to n
+
+result = 0
+for i in xrange(n+1):
+	#print i, result
+	result = i + result
+print "XSum:", result
+
+result = 0
+for i in range(n+1):
+	#print i, result
+	result = i + result
+print "Sum:", result
+
+
---
+++ simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def h(name):
+	print "hello", name, ", how are you?"
+
+
+
+
+h("dave")
+h("saskia")
+h("liam")
+h("andrew")
